51ca7b2cb0a03e07df8e08536ab6a4d07b09104e
Tiago <tiago.venceslau@pdmfc.com>
2022-06-08 15:54:26 +0100

    refs #1 - re changes the event name

--- src/code/scripts/controllers/HomeController.js
+++ src/code/scripts/controllers/HomeController.js
@@ -164,7 +164,7 @@ export default class HomeController extends WebcController{
     }
 
     report(status, error){
-        const event = new CustomEvent('windowAction', {
+        const event = new CustomEvent('ssapp-action', {
             bubbles: true,
             cancelable: true,
             detail: new AuthFeatureResponse(status, error)
